bf471396766e25009aa8810835e155d43789c584
Lee Jaeseong <fg9658@gmail.com>
2021-08-24 19:26:22 +0900

    fix: Swagger always DEBUG mode

--- stockers/settings.py
+++ stockers/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 from global_variable import DATABASES, SECRET_KEY, IS_DEBUG
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# TODO: 스웨거 문서 뜨게 변경 후 삭제 필요
 DEBUG = IS_DEBUG or True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,9 +122,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
@@ -159,7 +156,3 @@ CORS_ALLOW_HEADERS = (
     'x-csrftoken',
     'x-requested-with',
 )
-
-SWAGGER_SETTINGS = {
-   'USE_SESSION_AUTH': False,
-}
--- stockers/urls.py
+++ stockers/urls.py
@@ -1,33 +1,28 @@
 from django.urls import path, re_path, include
-from django.conf import settings
-
-from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg       import openapi
-
-
 
 urlpatterns = [
     path('users', include('users.urls')),
     path('applications', include('applications.urls')),
     path('recruits', include('recruits.urls')),
     path('', include('helloworld.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
-schema_view = get_schema_view( 
-    openapi.Info( 
-        title            = "Stockers API", 
-        default_version  = "v1", 
-        description      = "Stockers API 문서", 
-        terms_of_service = "https://www.google.com/policies/terms/", 
-        contact          = openapi.Contact(email="test@test.com"), 
-        license          = openapi.License(name="김예랑, 최명준"), 
-    ), 
+schema_view = get_schema_view(
+    openapi.Info(
+        title            = "Stockers API",
+        default_version  = "v1",
+        description      = "Stockers API 문서",
+        terms_of_service = "https://www.google.com/policies/terms/",
+        contact          = openapi.Contact(email="test@test.com"),
+        license          = openapi.License(name="김예랑, 최명준"),
+    ),
 
-    public             = True, 
-    permission_classes = (permissions.AllowAny,), 
+    public             = True,
+    permission_classes = (permissions.AllowAny,),
 )
 
 urlpatterns += [
